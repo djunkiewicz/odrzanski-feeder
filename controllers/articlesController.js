53c2d5535d9b0fbb3ba1e75c968b2ff93ea2d1fa
Damian Junkiewicz <junkiewicz.damian@gmail.com>
2024-12-09 14:37:17 +0100

    Add: photo_paths field provided for articleCard

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -1,8 +1,31 @@
+const path = require("node:path");
 const articlesRepository = require("../repositories/articlesRepository");
+const fs = require("node:fs/promises");
 
 async function getAllArticles() {
   const result = await articlesRepository.getAllArticles();
+  for (const article of result) {
+    article.photo_paths = await getPhotoPaths(article.gallery_path);
+  }
+  console.log(result);
   return JSON.stringify(result);
 }
 
 module.exports = { getAllArticles };
+
+async function getPhotoPaths(directory) {
+  try {
+    const files = await fs.readdir("public/" + directory);
+    const pathList = [];
+    if (files.length == 0) {
+      pathList.push("images/no_photo.jpg");
+    } else {
+      files.forEach((file) => {
+        pathList.push(directory + "/" + file);
+      });
+    }
+    return pathList;
+  } catch (err) {
+    console.log(err);
+  }
+}
--- public/components/articleCard.js
+++ public/components/articleCard.js
@@ -6,6 +6,7 @@ class Article {
     this.galleryPath = data.gallery_path;
     this.creationDate = data.creation_date;
     this.metaKeywords = data.meta_keywords;
+    this.photoPaths = data.photo_paths;
   }
 
   getCard() {
@@ -15,15 +16,15 @@ class Article {
     const title = document.createElement("h2");
     title.textContent = this.name;
 
+    const image = document.createElement("img");
+    image.src = this.photoPaths[0];
+
     const content = document.createElement("p");
     content.textContent = this.content;
 
-    const image = document.createElement("p");
-    image.textContent = this.galleryPath;
-
     articleCard.appendChild(title);
-    articleCard.appendChild(content);
     articleCard.appendChild(image);
+    articleCard.appendChild(content);
 
     return articleCard;
   }
