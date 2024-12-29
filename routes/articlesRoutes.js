d1d09ef2a9944c841a6d3ed56ff7637d6b7511ed
Damian Junkiewicz <junkiewicz.damian@gmail.com>
2024-12-29 22:04:44 +0100

    Articles routes refactor

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -1,6 +1,11 @@
 const articlesController = require("../controllers/articlesController");
 
 module.exports = function (app) {
+  app.get("/", async (req, res) => {
+    const articles = await articlesController.getAllArticles();
+    res.render("articles.ejs", { articles: articles });
+  });
+
   app.get("/article", async (req, res) => {
     const articles = await articlesController.getAllArticles();
     res.render("articles.ejs", { articles: articles });
