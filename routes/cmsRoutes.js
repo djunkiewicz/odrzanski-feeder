4abfb47259f3df1ea8ec0b7462b5893b5deb51fb
Damian Junkiewicz <junkiewicz.damian@gmail.com>
2025-01-01 20:06:42 +0100

    Add: initial 'upload file' handling - TODO

--- routes/cmsRoutes.js
+++ routes/cmsRoutes.js
@@ -1,9 +1,24 @@
+const multer = require("multer");
+const path = require("path");
+
 const checkAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
   }
   res.redirect("/cms/login");
 };
+
+// Konfiguracja Multer
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/images/A_test_storage"); // Ścieżka do folderu, w którym będą zapisane pliki
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname)); // Nadaj unikalną nazwę pliku
+  },
+});
+const storage2 = multer.memoryStorage(); // Pliki będą przechowywane w pamięci
+const upload = multer({ storage: storage2 });
 
 module.exports = function (app, passport) {
   //authentication, login and logout START
@@ -41,4 +56,19 @@ module.exports = function (app, passport) {
   app.get("/cms/competitions", checkAuthenticated, (req, res) => {
     res.render("./cms/");
   });
+
+  app.post(
+    "/cms/articles/new",
+    checkAuthenticated,
+    upload.array("images"),
+    (req, res) => {
+      const data1 = req.body;
+      const data2 = req.files;
+      console.log("Text data:");
+      console.log(data1);
+      console.log("Files:");
+      console.log(data2);
+      res.redirect("/cms/articles");
+    }
+  );
 };
